91cbdca61f6ec546326aefa321313f65a7d600fd
manoel jailton <manoeljailton79@gmail.com>
2023-12-12 22:16:48 -0300

    product-list-base

--- src/components/Avaliacao/index.tsx
+++ src/components/Avaliacao/index.tsx
@@ -1,7 +1,7 @@
-const avaliacao = () => (
+const Avaliacao = () => (
   <div>
     <span>4.9</span>
     <img src="//placehold.it/21x21" alt="" />
   </div>
 )
-export default avaliacao
+export default Avaliacao
